6d129b6d94d4c1bb9947bcfc14c17920ae9eb3cb
Luz <devluz@live.de>
2016-05-15 15:12:28 +1200

    Added forum link

--- modinfo.lua
+++ modinfo.lua
@@ -1,9 +1,9 @@
 name = "Shitty Explorer"
-description = "This mod allows you to discover the whole map (including caves) by moving your character accross the map. Open the map and then use console command 'shittyexplorer()'. Ideally use c_godmode() before to avoid diing while exploring the map."
+description = "This mod allows you to discover the whole map (including caves) by automatically moving your character accross the map. Open the map and then use console command 'shittyexplorer()'. Ideally use c_godmode() before to avoid dying while exploring the map."
 author = "devluz"
 version = "0.1"
 
-forumthread = ""
+forumthread = "http://forums.kleientertainment.com/topic/67302-tyexplorer-mod-thread/"
 
 api_version = 10
 
